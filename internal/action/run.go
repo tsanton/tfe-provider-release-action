0ce52f69bd8b75bf879512b51602651cfbb8fcaf
Tobias Antonsen <tobias@tsant.no>
2023-04-23 12:11:19 +0000

    feat: added logging

--- internal/action/run.go
+++ internal/action/run.go
@@ -21,10 +21,13 @@ func Run(cli *api.TerraformEnterpriseClient, logger u.ILogger, config *RunConfig
 
 	// 1. Check if the provider version exists. If it does not, create a provider version
 	var providerVersion *aresp.ProviderVersion
+	logger.Infof("Checking if provider version %s exists", config.GoreleaserMetadata.Version)
 	providerVersion, err := checkProviderVersionExists(ctx, cli, config) //Get -> return nil if 404
 	if err != nil {
+		logger.Info("Error checking if provider version exists")
 		return err //TODO: custom error message
 	} else if providerVersion == nil {
+		logger.Infof("Creating provider version %s", config.GoreleaserMetadata.Version)
 		providerVersion, err = createProviderVersion(ctx, cli, config)
 		if err != nil {
 			logger.Errorf("Error creating provider version:", err)
@@ -34,8 +37,10 @@ func Run(cli *api.TerraformEnterpriseClient, logger u.ILogger, config *RunConfig
 
 	// 2. Upload the SHA256SUMS and SHA256SUMS.sig files if not uploaded:
 	if !providerVersion.Data.Attributes.ShasumsUploaded {
+		logger.Info("Uploading SHA256SUMS file")
 		fileContent, err := ReadFileToByteArray(config.Checksum.Path)
 		if err != nil {
+			logger.Infof("Error reading SHA256SUMS file from path: %s", config.Checksum.Path)
 			return err
 		}
 		request, err := CreateRequestFromByteArray(http.MethodPost, providerVersion.Data.Links.ShasumsUploadUrl, fileContent, config.Checksum.Name)
@@ -46,30 +51,38 @@ func Run(cli *api.TerraformEnterpriseClient, logger u.ILogger, config *RunConfig
 		request.Header.Set("Accept", "application/json")
 		_, err = api.Do[interface{}](ctx, cli, 200, request)
 		if err != nil {
+			logger.Info("Error Uploading SHA256SUMS file")
 			return err
 		}
 	}
 	if !providerVersion.Data.Attributes.ShasumsSigUploaded {
+		logger.Info("Uploading SHA256SUMS signature file")
 		fileContent, err := ReadFileToByteArray(config.Signature.Path)
 		if err != nil {
+			logger.Infof("Error reading SHA256SUMS signature file from path: %s", config.Signature.Path)
 			return err
 		}
 		request, err := CreateRequestFromByteArray(http.MethodPost, providerVersion.Data.Links.ShasumsSigUploadUrl, fileContent, config.Signature.Name)
 		if err != nil {
 			return err
 		}
+		request.Header.Set("Content-Type", "application/vnd.api+json")
+		request.Header.Set("Accept", "application/json")
 		_, err = api.Do[interface{}](ctx, cli, 200, request)
 		if err != nil {
+			logger.Info("Error Uploading SHA256SUMS signature file")
 			return err
 		}
 	}
 
 	// 3. Get provider platform for the version
+	logger.Info("Getting existing provider platform versions")
 	providerPlatforms, err := getProviderVersionPlatforms(ctx, cli, config)
 	if err != nil {
 		return err
 	}
 	for _, platform := range config.ProviderVersionPlatforms {
+		logger.Infof("Processing provider platform version: %s %s", platform.Os, platform.Goarch)
 		var tfplatform *aresp.ProviderVersionPlatformData
 		var exists bool
 		// 4. Check if the provider platform exists. If it does not, create a provider platform (exist by match on os + arch, and provider-binary-uploaded: false)
@@ -77,9 +90,10 @@ func Run(cli *api.TerraformEnterpriseClient, logger u.ILogger, config *RunConfig
 			return i.Attributes.Os == platform.Os && i.Attributes.Arch == platform.Goarch
 		})
 		if !exists {
+			logger.Infof("Creating provider platform version: %s %s", platform.Os, platform.Goarch)
 			tfplatform, err = createProviderPlatformVersion(ctx, cli, config, platform)
 			if err != nil {
-				logger.Errorf("Error creating provider version platform %s %s: %s", platform.Os, platform.Goarch, err)
+				logger.Errorf("Error creating provider version platform %s %s", platform.Os, platform.Goarch)
 				return err
 			}
 		} else {
@@ -88,8 +102,10 @@ func Run(cli *api.TerraformEnterpriseClient, logger u.ILogger, config *RunConfig
 		// 5. Upload the provider binary zip file if not uploaded
 		// - when provider-binary-uploaded: false, instead of including a link to provider-binary-download, the response will include an upload link provider-binary-upload
 		if !tfplatform.Attributes.ProviderBinaryUploaded {
+			logger.Infof("Uploading provider platform binary: %s %s", platform.Os, platform.Goarch)
 			fileContent, err := ReadFileToByteArray(platform.Path)
 			if err != nil {
+				logger.Infof("Error reading provider platform binary at path: %s", platform.Path)
 				return err
 			}
 			request, err := CreateRequestFromByteArray(http.MethodPost, tfplatform.Links.ProviderBinaryUpload, fileContent, platform.Name)
@@ -98,8 +114,10 @@ func Run(cli *api.TerraformEnterpriseClient, logger u.ILogger, config *RunConfig
 			}
 			_, err = api.Do[interface{}](ctx, cli, 200, request)
 			if err != nil {
+				logger.Info("Error Uploading provider platform binary")
 				return err
 			}
+			logger.Info("Successfully uploaded provider platform binary")
 		}
 	}
 	logger.Info("Provider version and platform created/uploaded successfully!")
